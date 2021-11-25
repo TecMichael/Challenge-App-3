0d1c85ceb2d6206b30e604c9930a0771030ce6ff
Michael Agbaje <seyimichael16@gmail.com>
2021-11-25 19:23:48 +0800

    ch

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,28 @@
 import 'package:challenge_app_3/components/active_filters.dart';
-import 'package:challenge_app_3/constants/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<String> _icon = [
+    // 'assets/tesla.svg',
+    'assets/audi.png',
+    'assets/audi.png',
+    'assets/mit.png'
+  ];
+
+  final List<String> _brand = ['Tesla', 'Audi', 'Mitsubishi'];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff171c22),
       appBar: AppBar(
@@ -72,28 +85,69 @@ class HomeScreen extends StatelessWidget {
                 ),
               ],
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(
-                    right: 130,
-                    bottom: 1,
-                    left: 25,
-                  ),
+            const SizedBox(height: 12),
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height,
+              padding: const EdgeInsets.all(12),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(26),
+                  topRight: Radius.circular(26),
                 ),
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.all(12),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(22),
-                      topRight: Radius.circular(22),
-                    ),
-                  ),
+              ),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ...List.generate(
+                      3,
+                      (index) => GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _currentIndex = index;
+                          });
+                        },
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.all(8),
+                              // height: 140,
+                              width: 140,
+                              decoration: BoxDecoration(
+                                color: _currentIndex == index
+                                    ? const Color(0xffc5cad4)
+                                    : const Color(0xfff9f9f9),
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(14.0),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Image.asset(_icon[index]),
+                                    Text(
+                                      _brand[index],
+                                      style: const TextStyle(
+                                          color: Color(
+                                            0xff272729,
+                                          ),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15.5),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-              ],
+              ),
             ),
           ],
         ),
